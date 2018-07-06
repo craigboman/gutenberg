4ffdcb18294f850dd82e781425c075e88fd0a075
Craig Boman <craig.boman@gmail.com>
2018-07-06 18:03:59 +0000

    tutorial

---
+++ republic/process_doc.py
@@ -0,0 +1,34 @@
+#import string
+
+# load doc into memory
+def load_doc(filename):
+	# open the file as read only
+	file = open(filename, 'r')
+	# read all text
+	text = file.read()
+	# close the file
+	file.close()
+	return text
+
+# turn a doc into clean tokens
+def clean_doc(doc):
+	# replace '--' with a space ' '
+	doc = doc.replace(' ', ' ')
+	# split into tokens by white space
+	tokens = doc.split()
+	# remove punctuation from each token
+	table = str.maketrans('', '', string.punctuation)
+	tokens = [w.translate(table) for w in tokens]
+	# remove remaining tokens that are not alphabetic
+	tokens = [word for word in tokens if word.isalpha()]
+	# make lower case
+	tokens = [word.lower() for word in tokens]
+	return tokens
+
+# save tokens to file, one dialog per line
+def save_doc(lines, filename):
+	data = '\n'.join(lines)
+	file = open(filename, 'w')
+	file.write(data)
+	file.close()
+
