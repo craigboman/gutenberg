e8314b37f6dc025bdfcde71d6efbd06a7b153def
Dude <craig.boman@gmail.com>
2018-08-24 15:50:39 -0400

    loop

--- python/loopSeq.py
+++ python/loopSeq.py
@@ -1,13 +1,9 @@
 import os
-from process_doc import load_doc, clean_doc, save_doc, loadClean
+from jsn import pick
 
 
-def loopSeq:
-    #function to loop through PG ebooks and output sequence.txt files 
-    dir = '/home/science/gutenberg/archive'    
-    files = os.listdir()
 
-    for x in files: 
-	print x
-	load_doc(x) #running into char encoding errors 37815-8.txt; use iconv to convert encoding
-		
+def loop()
+    books = os.listdir()
+    for i in books:
+        pick(i)
